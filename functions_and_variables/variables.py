2473e64345393da366ab9f6b949958690ce314a1
ISHRAQ <izrumac@gmail.com>
2026-05-12 21:55:14 +0530

    STR

---
+++ functions_and_variables/variables.py
@@ -0,0 +1,55 @@
+# ------------VAR--------------
+# STR
+
+
+# #input and output
+# name = str(input("whats your name :"))
+# print("Hey",name)
+# age = int(input("your age: "))
+# print("Your age is" , age)
+
+
+# #combined command and variables 
+# print(f"your name is {name} and you are {age} year old")
+
+
+# # \"---------\"
+# print("your name is \"ishraq\"")
+
+
+# # Remove extra spaces from left and right
+# a = "       JOHN & JACOB        "
+# print(a)
+
+
+# a = a.rstrip()                # remove space from right                       
+# print(a)
+
+# a = a.strip()                 # remove spaces from both
+# print(a)
+
+# a = a.lstrip()                # remove spaced from left
+# print(a)
+
+
+# # capitalize() title() upper() lower()
+# b = "python PROGRAMMING"
+
+# b = b.capitalize()          # capital first letter the first word
+# print(b)
+
+# b = b.title()               # capital first letter of all word
+# print(b)
+
+# b = b.upper()
+# print(b)
+
+# b = b.lower()
+# print(b)
+
+# b = b.split()               # it split and converted into list contains the splitted items
+# print(b)
+
+
+# INT
+
